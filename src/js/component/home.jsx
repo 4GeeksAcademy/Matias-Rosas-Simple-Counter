52fa21503b7ac1435d1932f97f0c11d026d00cd4
MatiRosas31 <165863634+MatiRosas31@users.noreply.github.com>
2024-10-04 21:02:45 +0000

    updated

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,40 @@
 import React from "react";
 
+import '../../styles/index.css'
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="row divcontainer">
+			<div className="col column ms-3" ><i className="far fa-clock"></i></div>
+			<div className="col column" >{digitfive}</div>
+			<div className="col column" >{digitfour}</div>
+			<div className="col column" >{digitthree}</div>
+			<div className="col column" >{digittwo}</div>
+			<div className="col column" >{digitone}</div>
+
 		</div>
 	);
 };
+
+
+const digitone = 0
+const digittwo = 0
+const digitthree = 0
+const digitfour = 0
+const digitfive = 0
+
+
+// SetInterval(function(){
+// 	if (digitone < 10)
+// 		digitone++
+// 	else {
+// 		digitone === 0
+// 	}
+
+
+// })
+
 
 export default Home;
