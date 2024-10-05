e2d9d8dd788fd22bc4605d7b78b7886417fb7d74
MatiRosas31 <165863634+MatiRosas31@users.noreply.github.com>
2024-10-05 01:07:13 +0000

    [Cambios radicales: proyecto completado]
    -Se cambiaron los if states para cumplir con la
    consigna del proyecto
    -Se agregaron botones de pausa y resumir
    -Se agrego un form para que el usuario pueda
    introducir una cantidad de segundos
    -Se agrego una alerta que aparece cuando el
    usuario ingresa un determinado numero de segundos
    en el form

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,40 +1,40 @@
 import React from "react";
+import '../../styles/index.css';
 
-import '../../styles/index.css'
-//include images into your bundle
+// Variable global para almacenar el valor ingresado por el usuario
+let userInput = null;
 
-//create your first component
 const Home = (props) => {
-	return (
-		<div className="row divcontainer">
-			<div className="col column ms-3" ><i className="far fa-clock"></i></div>
-			<div className="col column" >{digitfive}</div>
-			<div className="col column" >{digitfour}</div>
-			<div className="col column" >{digitthree}</div>
-			<div className="col column" >{digittwo}</div>
-			<div className="col column" >{digitone}</div>
-
-		</div>
-	);
+    return (
+        <>
+        <div className="row divcontainer">
+            <div className="col column ms-3" ><i className="far fa-clock"></i></div>
+            <div id="five" className="col column">{props.digitFive}</div>
+            <div id="four" className="col column">{props.digitFour}</div>
+            <div id="three" className="col column">{props.digitThree}</div>
+            <div id="two" className="col column">{props.digitTwo}</div>
+            <div id="one" className="col column">{props.digitOne}</div>
+            <button className="btn btn-danger" onClick={() => { props.Stop() }}>Stop</button>
+            <button className="btn btn-success" onClick={() => { props.Resume() }}>Resume</button>
+        </div>
+        <form>
+            <label htmlFor="usertimerinput">Set Time (seconds): </label>
+            <input type="number" id="usertimerinput" />
+            <button
+                type="button"
+                onClick={() => {
+					const inputElement = document.getElementById("usertimerinput");
+					if (inputElement) {
+						props.setUserInput(Number(inputElement.value));
+						console.log("Button was clicked, userInput set to: " + inputElement.value);
+					}
+				}}
+            >
+                Set Timer
+            </button>
+        </form>
+        </>
+    );
 };
-
-
-const digitone = 0
-const digittwo = 0
-const digitthree = 0
-const digitfour = 0
-const digitfive = 0
-
-
-// SetInterval(function(){
-// 	if (digitone < 10)
-// 		digitone++
-// 	else {
-// 		digitone === 0
-// 	}
-
-
-// })
-
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,53 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// include your styles into the webpack bundle
 import "../styles/index.css";
-
-//import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+let seconds = 0;
+let processInterval;
+let userInput = null; // Variable global para almacenar el valor ingresado por el usuario
+const root = ReactDOM.createRoot(document.getElementById('app'));
 
+const Stop = () => {
+    clearInterval(processInterval);
+};
+
+const interval = function () {
+    processInterval = setInterval(function () {
+        if (userInput !== null && seconds === userInput) {
+            console.log("si aparece esto es que la alerta funciona")
+            alert("Time reached: " + userInput + " seconds");
+            Stop();
+        }
+
+        const digitfive = Math.floor(seconds / 10000);
+        const digitfour = Math.floor(seconds / 1000);
+        const digitthree = Math.floor(seconds / 100);
+        const digittwo = Math.floor(seconds / 10);
+        const digitone = Math.floor(seconds / 1);
+
+        root.render(
+            <Home
+                digitOne={digitone % 10}
+                digitTwo={digittwo % 10}
+                digitThree={digitthree % 10}
+                digitFour={digitfour % 10}
+                digitFive={digitfive % 10}
+                Stop={Stop}
+                Resume={Resume}
+                setUserInput={(value) => {
+                    userInput = value;
+                }}
+            />
+        );
+
+        seconds++;
+    }, 1000);
+};
+
+interval();
+
+const Resume = () => {
+    interval();
+};
