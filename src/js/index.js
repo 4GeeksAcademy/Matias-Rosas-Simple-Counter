5301998ef83784184158fc7c9083d565f7fc5914
MatiRosas31 <165863634+MatiRosas31@users.noreply.github.com>
2024-10-07 21:22:49 +0000

    [Multiples intervalos bug arreglado] -El bug ha sido arreglado. Cambie el valor de la variable processInterval a "null". -Agregue un if que evaluara si processInterval era "null" y de ser True iniciara todo el contador. Con el fin de prevenir que se inicie un nuevo intervalo si hay uno activo. En la function Resume tambien agregue el mismo If que evalua  lo mismo.

--- src/js/index.js
+++ src/js/index.js
@@ -5,49 +5,55 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 let seconds = 0;
-let processInterval;
+let processInterval = null;
 let userInput = null; // Variable global para almacenar el valor ingresado por el usuario
 const root = ReactDOM.createRoot(document.getElementById('app'));
 
 const Stop = () => {
     clearInterval(processInterval);
+    processInterval = null;
 };
 
 const interval = function () {
-    processInterval = setInterval(function () {
-        if (userInput !== null && seconds === userInput) {
-            console.log("si aparece esto es que la alerta funciona")
-            alert("Time reached: " + userInput + " seconds");
-            Stop();
-        }
+    if (processInterval === null) { // Solo inicia un nuevo intervalo si no hay uno activo
+        processInterval = setInterval(function () {
+            if (userInput !== null && seconds === userInput) {
+                console.log("si aparece esto es que la alerta funciona");
+                alert("Time reached: " + userInput + " seconds");
+                Stop();
+            }
 
-        const digitfive = Math.floor(seconds / 10000);
-        const digitfour = Math.floor(seconds / 1000);
-        const digitthree = Math.floor(seconds / 100);
-        const digittwo = Math.floor(seconds / 10);
-        const digitone = Math.floor(seconds / 1);
+            const digitfive = Math.floor(seconds / 10000);
+            const digitfour = Math.floor(seconds / 1000);
+            const digitthree = Math.floor(seconds / 100);
+            const digittwo = Math.floor(seconds / 10);
+            const digitone = Math.floor(seconds / 1);
 
-        root.render(
-            <Home
-                digitOne={digitone % 10}
-                digitTwo={digittwo % 10}
-                digitThree={digitthree % 10}
-                digitFour={digitfour % 10}
-                digitFive={digitfive % 10}
-                Stop={Stop}
-                Resume={Resume}
-                setUserInput={(value) => {
-                    userInput = value;
-                }}
-            />
-        );
+            root.render(
+                <Home
+                    digitOne={digitone % 10}
+                    digitTwo={digittwo % 10}
+                    digitThree={digitthree % 10}
+                    digitFour={digitfour % 10}
+                    digitFive={digitfive % 10}
+                    Stop={Stop}
+                    Resume={Resume}
+                    setUserInput={(value) => {
+                        userInput = value;
+                    }}
+                />
+            );
 
-        seconds++;
-    }, 1000);
+            seconds++;
+        }, 1000);
+    }
 };
 
 interval();
 
 const Resume = () => {
-    interval();
+    if (processInterval === null) {
+        interval();
+ }
+
 };
